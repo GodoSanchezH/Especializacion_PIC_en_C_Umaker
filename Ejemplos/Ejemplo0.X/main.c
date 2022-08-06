0a5bdb3460522b3789f2fcd06bb98f984733c31e
Godo Sanchez Heredia <gssanchezh@ieee.org>
2022-08-06 18:11:15 -0500

    v1

---
+++ Ejemplos/Ejemplo0.X/main.c
@@ -0,0 +1,30 @@
+
+#include "mcc_generated_files/mcc.h"
+
+/*
+                         Main application
+ */
+void main(void)
+{
+    // Initialize the device
+    SYSTEM_Initialize();
+
+    // If using interrupts in PIC18 High/Low Priority Mode you need to enable the Global High and Low Interrupts
+    // If using interrupts in PIC Mid-Range Compatibility Mode you need to enable the Global Interrupts
+    // Use the following macros to:
+
+    // Enable the Global Interrupts
+    //INTERRUPT_GlobalInterruptEnable();
+
+    // Disable the Global Interrupts
+    //INTERRUPT_GlobalInterruptDisable();
+
+    while (1)
+    {
+        LED0_Toggle();
+        __delay_ms(400);
+    }
+}
+/**
+ End of File
+*/
