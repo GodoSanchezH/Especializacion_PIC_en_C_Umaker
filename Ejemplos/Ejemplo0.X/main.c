ddf14500fb2199c95a33d6264937ef0296b98017
Godo Sanchez Heredia <gssanchezh@ieee.org>
2022-08-06 18:56:56 -0500

    v1

--- Ejemplos/Ejemplo0.X/main.c
+++ Ejemplos/Ejemplo0.X/main.c
@@ -7,8 +7,10 @@
 void main(void)
 {
     // Initialize the device
+    asm("nop");
+    
     SYSTEM_Initialize();
-
+        
     // If using interrupts in PIC18 High/Low Priority Mode you need to enable the Global High and Low Interrupts
     // If using interrupts in PIC Mid-Range Compatibility Mode you need to enable the Global Interrupts
     // Use the following macros to:
