77bdf0781a2d396a1ca7e287e759c01722ab5d62
Godo Sanchez Heredia <gssanchezh@ieee.org>
2022-09-25 19:37:15 -0500

    v1

---
+++ Ejemplo14_PID.X/build/default/production/mcc_generated_files/tmr0.p1.d
@@ -0,0 +1,7 @@
+build/default/production/mcc_generated_files/tmr0.p1:  \
+mcc_generated_files/tmr0.c  \
+mcc_generated_files/tmr0.h  \
+mcc_generated_files/pin_manager.h  \
+mcc_generated_files/uart1.h  \
+mcc_generated_files/pwm1.h  \
+mcc_generated_files/../PID.h 
---
+++ Ejemplo14_PID.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=Ejemplo14_PID.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/Ejemplo14_PID.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=ejemplo14pid.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/ejemplo14pid.x.tar
